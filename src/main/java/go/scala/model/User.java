3b7384feec37c36250c08ff87d4e720ddebe8540
Stein Kåre Skytteren <skytteren@stein-kare-skytterens-macbook-pro.local>
2010-11-11 22:39:51 +0100

    Init with toString, equals and hashcode

--- src/main/java/go/scala/model/User.java
+++ src/main/java/go/scala/model/User.java
@@ -48,6 +48,34 @@ public class User {
 	public void setBlogs(List<Blog> blogs) {
 		this.blogs = blogs;
 	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "User [blogs=" + blogs + ", email=" + email + ", id=" + id
+				+ ", name=" + name + ", password=" + password + "]";
+	}
 
 	
 }
